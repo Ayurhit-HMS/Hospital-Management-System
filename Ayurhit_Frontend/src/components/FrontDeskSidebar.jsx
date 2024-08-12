64871c5e7f8641b35c25a22bd955466d432e8579
Rohit <rohitbagal166@gmail.com>
2024-08-12 11:49:50 +0530

    Sidebar component of users

---
+++ Ayurhit_Frontend/src/components/FrontDeskSidebar.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react';
+import "../styles/sidebar.css"
+
+function FrontDeskSidebar() {
+
+    useEffect(() => {
+        const offcanvasElement = document.getElementById('offcanvasScrolling');
+        const bsOffcanvas = new window.bootstrap.Offcanvas(offcanvasElement);
+        bsOffcanvas.show();
+    }, []);
+
+    const handleShow = () => {
+        const offcanvasElement = document.getElementById('offcanvasScrolling');
+        const bsOffcanvas = new window.bootstrap.Offcanvas(offcanvasElement);
+        bsOffcanvas.show();
+    };
+    return (
+        <div>
+            <button className="btn btn-secondary menu-button ms-1" type="button" onClick={handleShow} aria-controls="offcanvasScrolling">
+                <i className="fas fa-bars"></i>
+            </button>
+            <div className="offcanvas offcanvas-start custom-canvas" data-bs-scroll="true" data-bs-backdrop="false" tabIndex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel" style={{ width: 300 }}>
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title" id="offcanvasScrollingLabel"></h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    <div className="container d-grid">
+                        <button className="btn btn-custom mb-2">Dashboard</button>
+                        <button className="btn btn-custom mb-2">Appointment Reminders</button>
+                        <button className="btn btn-custom mb-2">Schedule Appointment</button>
+                        <button className="btn btn-custom mb-2">Patient Registration</button>
+                        <button className="btn btn-custom mb-2">Settings</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FrontDeskSidebar;
